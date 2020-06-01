f9d1ecd629f86cb02ad865a1040457a452285ff5
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2020-06-01 18:53:25 -0500

    feat: added example component and linting

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import HelloWorld from "./components/HelloWorld";
 import "./App.css";
 
 function App() {
-  return <div className="App"> Project Metroid </div>;
+  return (
+    <div className="App">
+      Project Metroid
+      <HelloWorld />
+    </div>
+  );
 }
 
 export default App;
