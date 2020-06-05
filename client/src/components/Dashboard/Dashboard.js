09ae956ac7e2e0dfc16c3455cf5336f26d6422f2
i-k0n <19754246+i-k0n@users.noreply.github.com>
2020-06-04 22:27:05 -0500

    feat: added list of popular games

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,49 +1,32 @@
 import React from "react";
 import Notice from "../Notice";
 import InfoCard from "../InfoCard";
-import styled from "@emotion/styled";
-
-const infoCardData = [
-  {
-    icon: `fas fa-clipboard-list`,
-    copy:
-      "Keep track of all the games you’ve played or start from the day you join.",
-  },
-  {
-    icon: `fas fa-gamepad`,
-    copy:
-      "Start a video game collection and keep track of all the games you love!",
-  },
-  {
-    icon: `fas fa-star-half-alt`,
-    copy: "Tell others how you felt about a game by reviewing it.",
-  },
-  {
-    icon: `fas fa-heart`,
-    copy: "Share your love for a game by leaving a like!",
-  },
-  {
-    icon: `fas fa-book-open`,
-    copy:
-      "Keep a gaming journal about tips, tricks and easter eggs you’ve found.",
-  },
-];
-
-const InfoCardWrapper = styled.div`
-  display: flex;
-  overflow-x: auto;
-  margin: 0 1em;
-`;
+import { CardRow, RowTitle } from "./Dashboard.styles";
+import { default as popularGameCardData } from "./popularGameCardData.json";
+import { default as infoCardData } from "./infoCardData.json";
+import GameCard from "../GameCard/GameCard";
 
 const Dashboard = () => {
   return (
     <div>
       <Notice />
-      <InfoCardWrapper>
+      <RowTitle>Popular</RowTitle>
+      <CardRow>
+        {popularGameCardData.map((card, i) => (
+          <GameCard
+            key={i}
+            img={card.img}
+            title={card.title}
+            developer={card.developer}
+          />
+        ))}
+      </CardRow>
+      <RowTitle>Project Metroid Lets You</RowTitle>
+      <CardRow>
         {infoCardData.map((card, i) => {
           return <InfoCard key={i} icon={card.icon} copy={card.copy} />;
         })}
-      </InfoCardWrapper>
+      </CardRow>
     </div>
   );
 };
