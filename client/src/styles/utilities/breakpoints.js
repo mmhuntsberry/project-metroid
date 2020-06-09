3b5e0d6d80de9506917473d870bd4518771ba130
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2020-06-09 11:40:49 -0500

    change max breakpoint

--- client/src/styles/utilities/breakpoints.js
+++ client/src/styles/utilities/breakpoints.js
@@ -5,7 +5,7 @@
 // xlarge 1224
 // max 1440
 
-const breakpoints = [320, 480, 768, 1024, 1200, 1224, 1824];
+const breakpoints = [320, 480, 768, 1024, 1200, 1224, 1440];
 
 const mq = breakpoints.map((bp) => `@media (min-width: ${bp}px)`);
 
