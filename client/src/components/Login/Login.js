509d548f45bc41b649e621c7694ebf45c0cd272b
andrew simon <andrewsimon0007@gmail.com>
2020-06-13 14:58:44 -0500

    Added a log in form

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Login = () => {
+    return (
+        // Add Css classes and submit button
+        <form class="form login-form" action="" method="">
+            <div class="container">
+                <h1>Log in to Project Metroid</h1>
+                
+                <label for="email"><b>Email</b></label> 
+                <input type="text" placeholder="" name="email" required></input>
+                
+                <label for="password"><b>Password</b></label> 
+                <input type="text" placeholder="" name="password" required></input>
+                
+                <a href="">Forgot Password?</a>
+            </div>
+
+            <div class="container">
+                {/Add the right route for sign up/}
+                <span>Don't have an account? <a href="#">Sign up</a></span>       
+                <button>LOG IN</button>
+            </div>
+        </form>
+    );
+};
