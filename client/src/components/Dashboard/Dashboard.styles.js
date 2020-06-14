c824a7e4a2f588685f4c8b639016787588a1f6db
i-k0n <19754246+i-k0n@users.noreply.github.com>
2020-06-14 14:13:23 -0500

    fix: organized styles, and moved vendor-prefixed styles to App.css

--- client/src/components/Dashboard/Dashboard.styles.js
+++ client/src/components/Dashboard/Dashboard.styles.js
@@ -1,43 +1,39 @@
 import styled from "@emotion/styled";
+import "../../styles/base/variables.css"
 
 const CardRow = styled.div `
   display: grid;
-    overflow-x: auto;
-    grid-template-columns: repeat(5, 1fr);
-    grid-gap: 1em;
-    margin: 0 1em;
+  grid-gap: 1em;
+  grid-template-columns: repeat(5, 1fr);
+  margin: 0 1em;
+  overflow-x: auto;
 `;
 
 const CardRowGrid = styled.div `
   display: grid;
-  grid-template-columns: repeat(2, 1fr) ;
   grid-gap: 1em;
+  grid-template-columns: repeat(2, 1fr);
   margin: 0 1em;
 
   @media (min-width: 600px) {
-    grid-template-columns: repeat(auto-fit, minmax(180px, 1fr) ) ;
-    max-width: 1440px;
+    grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
+    max-width: var(--max-breakpoint);
   }
 `
 
 const RowTitle = styled.h2 `
+  width: max-content;
+  margin-top: 2rem;
+  margin-bottom: 0.5rem;
+  margin-left: 1rem;
+  color: #28c7b7;
   font-size: 1.5em;
   font-weight: 500;
   line-height: 1.6;
-  color: #28c7b7;
-  width: -webkit-max-content;
-  width: -moz-max-content;
-  width: max-content;
-  background: linear-gradient(155.35deg, #1fee7e -28.1%, #2386bd 119.81%);
-  -webkit-text-fill-color: transparent;
-  -webkit-background-clip: text;
-  margin-top: 2rem;
-  margin-left: 1rem;
-  margin-bottom: 0.5rem;
 
   @media (min-width: 768px) {
-    font-size: 2em;
     margin-top: 3.5rem;
+    font-size: 2em;
   }
 `;
 
