b3d3d9623b9c09554bd810f0c2257f2a65d3add1
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2020-06-17 19:01:19 -0500

    fix: variable name typos

--- client/src/components/GamePage/game.styles.js
+++ client/src/components/GamePage/game.styles.js
@@ -19,22 +19,18 @@ export const HeroImage = styled.div`
   background-size: cover;
   box-shadow: inset 0 7px 9px -7px #000;
 
-  
-  ${mq[2]} {
-    
-  }
 `;
 
 export const HeroTextContainer = styled.div`
   display: flex;
-  justify-content: space-between;
   position: absolute;
   bottom: 0;
   align-items: baseline;
+  justify-content: space-between;
   margin-left: 16px;
 
   ${mq[2]} {
-    margin-left: 124px;
+    margin-left: calc(var(--layout-07) + var(--spacing-01));
   }
 `;
 
@@ -43,17 +39,15 @@ export const HeroTitle = styled.h1`
   color: var(--white);
   font-size: calc(1em + (3vw - 0.3vmin));
 
-  font-weight: var(--weight);
+  font-weight: var(--font-weight-light);
 `;
 
 export const HeroDetails = styled.span`
   display: none;
   margin: 0;
-  margin-left: 24px;
-  color: var(--white);
-  color: #979797;
-  font-size: 20px;
-  font-weight: var(--weight);
+  margin-left: var(--spacing-06);
+  color: var(--grey-05);
+  font-size: var(--body-font-size-05);
 
   ${mq[2]} {
     display: block;
@@ -61,34 +55,34 @@ export const HeroDetails = styled.span`
 
   &:nth-of-type(1)::after {
     content: "";
-    padding-right: 24px;
+    padding-right: var(--spacing-06);
     border-right: 2px solid #979797;
   }
 `;
 
 export const BodyContainer = styled.div`
   display: grid;
-  margin-left: 16px;
+  margin-left: var(--spacing-04);
   color: var(--white);
   line-height: 1.5;
 
   ${mq[2]} {
     grid-template-columns: 1fr 3fr;
-    margin-left: 124px;
+    margin-left: calc(var(--layout-07) + var(--spacing-01));
   }
 `;
 
 export const BoxArt = styled.img`
-  margin-top: 24px;
+  margin-top: var(-spacing-06);
   border-radius: 25px;
 `;
 
 export const GameDetailsContainer = styled.div`
-  margin-top: 24px;
-  margin-right: 20px;
+  margin-top: var(-spacing-06);
+  margin-right: var(-spacing-05);
 
   ${mq[2]} {
-    margin: 0 20px;
+    margin: 0 var(-spacing-05);
   }
   
   ${mq[6]} {
@@ -99,7 +93,7 @@ export const GameDetailsContainer = styled.div`
 export const GameSynopsis = styled.p``;
 
 export const GameDescription = styled.p`
-  margin-top: 24px;
+  margin-top: var(-spacing-06);
 `;
 
 export const GameTrailer = styled.iframe`
@@ -117,21 +111,21 @@ export const GameSidebar = styled.div``;
 export const GameActionWdiget = styled.div``;
 
 export const GameInfo = styled.div`
+  margin-top: var(-spacing-06);
   color: var(--white);
-  margin-top: 24px;
 `;
 
 export const InfoContainer = styled.div`
   display: grid;
+  grid-gap: var(--spacing-04) var(--spacing-02);
   grid-template-columns: auto 1fr;
   align-items: baseline;
-  grid-gap: 16px 8px;
 `;
 
 export const GameInfoTitle = styled.h4`
-  font-weight: var(--font-weight-light);
   display: inline;
   margin: 0;
+  font-weight: var(--font-weight-light);
 `;
 
 export const GameRating = styled.span`
@@ -142,26 +136,23 @@ export const GameRating = styled.span`
 export const GameGenre = styled.div``;
 export const GameTheme = styled.div``;
 export const GamePlatform = styled.div``;
-
-export const ReviewsContainer = styled.div`
-
-`;
+export const ReviewsContainer = styled.div``;
 
 export const SectionContainer = styled.div`
 display: flex;
 align-items: baseline;
 justify-content: space-between;
-margin-top: 48px;
+margin-top: var(--spacing-08);
 border-bottom: 1px solid var(--white);
 `;
 
 export const SectionTitle = styled.h3`
-font-weight: var(--font-weight-light);
+margin-bottom: var(--spacing-02);
 color: var(--dark-theme-bg-secondary);
-margin-bottom: 8px;
+font-weight: var(--font-weight-light);
 `;
 
 export const SectionLink = styled.a`
-  text-transform: uppercase;
   color: var(--dark-theme-bg-quaternary);
+  text-transform: uppercase;
 `;
