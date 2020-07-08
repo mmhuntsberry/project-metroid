f2649a9acab8bb9a32d07ee14e4c19183a147f54
coltonnewton89 <52718393+coltonnewton89@users.noreply.github.com>
2020-07-07 19:37:09 -0500

    commit_test_background_img_up

--- client/src/components/Browse/Browse.js
+++ client/src/components/Browse/Browse.js
@@ -1,54 +1,58 @@
 import React from "react";
 import GameCard from "../GameCard/GameCard";
-import { BrowseContainer } from "./Browse.styles";
+import { BrowseContainer, BackgroundImage } from "./Browse.styles";
+import './Browse.styles'
 import { CardRowGrid } from "../../styles/layout/layout.styles.js";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
 const Browse = () => {
-  return (
-    <Query query={GET_GAMES}>
-      {({ data, loading }) => {
-        console.log(data);
-        if (loading) return "Loading...";
+  
 
         return (
           <BrowseContainer>
-            <CardRowGrid>
-              {data &&
-                data.games &&
-                data.games.map((game) => (
-                  <GameCard
-                    id={game.id}
-                    key={game.id}
-                    img={game.box_art}
-                    title={game.title}
-                    developer={game.developer}
-                  />
-                ))}
-            </CardRowGrid>
+            <BackgroundImage/>
+            test
           </BrowseContainer>
-        );
-      }}
-    </Query>
+        
   );
 };
 
-const GET_GAMES = gql`
-  query {
-    games {
-      id
-      title
-      platform
-      release_year
-      box_art
-      synopsis
-      description
-      trailer
-      hero
-      developer
-    }
-  }
-`;
+
 
 export default Browse;
+
+
+// References //
+
+
+{/* <CardRowGrid>
+  {data &&
+    data.games &&
+    data.games.map((game) => (
+      <GameCard
+        id={game.id}
+        key={game.id}
+        img={game.box_art}
+        title={game.title}
+        developer={game.developer}
+      />
+    ))}
+  </CardRowGrid> */}
+
+//   const GET_GAMES = gql`
+//   query {
+//     games {
+//       id
+//       title
+//       platform
+//       release_year
+//       box_art
+//       synopsis
+//       description
+//       trailer
+//       hero
+//       developer
+//     }
+//   }
+// `;
--- client/src/components/Browse/Browse.styles.js
+++ client/src/components/Browse/Browse.styles.js
@@ -1,13 +1,31 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { mq } from "../../styles/utilities/breakpoints.js";
+import {
+  mq
+} from "../../styles/utilities/breakpoints.js";
 
-export const BrowseContainer = styled.div`
-margin-top: 124px;
-margin: 124px 16px 0;
+export const BrowseContainer = styled.div `
+
+
 
 ${mq[6]} {
-  margin: 124px 0 0;
+  
 
 }
+`;
+
+export const BackgroundImage = styled.div `
+  width: 100%;
+  height: 540px;
+  object-fit: cover;
+  background: linear-gradient(to bottom,
+    rgba(0, 0, 0, 0.5) 0%,
+    rgb(23, 24, 36, .5) 50%,
+    rgb(23, 24, 36, 1) 100%),
+    url("https://staticdelivery.nexusmods.com/images/3024/18420914-1576469743.jpg")
+    no-repeat;
+  background-size: cover;
+  background-position: center;
+  box-shadow: inset 0 7px 9px -7px #000;
+
 `;
