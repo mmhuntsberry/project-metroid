83ecf867268240e91dc01a3dfc4392df8a8e0e9d
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2020-07-12 10:32:08 -0500

    feat: update platforms games resolvers

--- server/src/resolvers/Platform.js
+++ server/src/resolvers/Platform.js
@@ -2,14 +2,12 @@ import { reduceFilter } from "../../utils/helpers.js";
 
 export const Platform = {
   async games(parent, args, ctx, info) {
-    // return reduceFilter(parent.games, db.games);
     const games = await ctx.prisma.games.findMany();
     const gamePlatforms = await ctx.prisma.game_platform.findMany();
 
     const filteredPlatform = games.reduce((filtered, curr) => {
       gamePlatforms.filter((item) => {
         if (parent.id === item.platform_id) {
-          console.log(curr);
           if (curr.id === item.game_id) {
             filtered.push(curr);
           }
@@ -18,8 +16,6 @@ export const Platform = {
 
       return filtered;
     }, []);
-
-    console.log("filteredPlatform", filteredPlatform);
 
     return filteredPlatform.map((p) => p);
   },
