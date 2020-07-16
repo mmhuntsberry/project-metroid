55dbdaca27cf0d7ee36d45a90cc76a5217b2a8f3
i-k0n <19754246+i-k0n@users.noreply.github.com>
2020-07-15 20:38:20 -0500

    feat: added App component

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Navbar from "./components/Navbar";
 import Dashboard from "./components/Dashboard";
 import Browse from "./components/Browse";
 import Discover from "./components/Discover";
+import Collections from "./components/Collections"
 import GamePage from "./components/GamePage/GamePage.tsx";
 import BottomNav from "./components/BottomNav";
 import Footer from "./components/Footer";
@@ -25,6 +26,7 @@ function App() {
           <Route exact path="/" component={Dashboard} />
           <Route exact path="/discover" component={Discover} />
           <Route exact path="/browse" component={Browse} />
+          <Route exact path="/collections" component={Collections} />
           <Route
             exact
             path="/games/:id"
