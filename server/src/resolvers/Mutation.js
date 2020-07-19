fe0f9d9134682443d393fff3f5c9fc66062cbb9a
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2020-07-19 11:19:29 -0500

    refactor: clean up create user mutation

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,17 +1,16 @@
 export const Mutation = {
-  async createUser(parent, args, ctx, info) {
-    const { data } = args;
+  async createUser(parent, { data }, ctx, info) {
     const { username, email, first_name, last_name, password } = data;
 
     const emailExists = await ctx.prisma.users.findOne({
       where: {
-        email: args.data.email,
+        email,
       },
     });
 
     const usernameExists = await ctx.prisma.users.findOne({
       where: {
-        username: args.data.username,
+        username,
       },
     });
 
@@ -29,46 +28,6 @@ export const Mutation = {
       throw new Error("Email or Username already exits choose another");
     }
   },
-  // createGame(parent, { title, platform, rating }, { db }, info) {
-  //   const titleTaken = db.games.some((game) => game.title === title);
-  //   const platformTaken = db.games.some((game) => game.platform === platform);
-
-  //   if (titleTaken && platformTaken) {
-  //     throw new Error("Game already exists");
-  //   }
-
-  //   const game = {
-  //     id: uuid(),
-  //     title,
-  //     platform,
-  //     rating: rating || 0,
-  //     reviews: [],
-  //   };
-
-  //   db.games.push(game);
-  //   return game;
-  // },
-  // createReview(parent, { user, review, game }, { db }, info) {
-  //   const userExists = db.users.some((u) => u.id === user);
-
-  //   if (!userExists) {
-  //     throw new Error("User not found");
-  //   }
-
-  //   const newReview = {
-  //     id: uuid(),
-  //     review,
-  //     game,
-  //     user,
-  //   };
-
-  //   const findUser = db.users.find((u) => u.id === user);
-  //   const findGame = db.games.find((g) => g.id === game);
-
-  //   findUser.reviews.push(newReview.id);
-  //   findGame.reviews.push(newReview.id);
-  //   db.reviews.push(newReview);
-
-  //   return newReview;
-  // },
+  createGame(parent, { title, platform, rating }, { db }, info) {},
+  createReview(parent, { user, review, game }, { db }, info) {},
 };
