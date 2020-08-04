901fad3a116e2514a9846cdafc8bb4f7affc6358
i-k0n <19754246+i-k0n@users.noreply.github.com>
2020-08-04 17:59:41 -0500

    added more dummy data to test pagination

---
+++ client/src/components/Collections/collectionListData.js
@@ -0,0 +1,105 @@
+// hard-coded data that will eventually be filled by the database
+const collectionListData = [
+  {
+    id: "0",
+    title: "All Games",
+    thumbnail: "https://gamefaqs1.cbsistatic.com/box/4/1/0/48410_front.jpg",
+    games: "381",
+    comments: "",
+    dateAdded: "2020-03-19 19:45:26",
+    isMain: true,
+    disabled: "disabled"
+  },
+  {
+    id: "1",
+    title: "Totally Rad Games",
+    thumbnail: "https://giantbomb1.cbsistatic.com/uploads/scale_medium/8/81005/2771287-2900330282-label.jpg",
+    games: "52",
+    comments: "12",
+    dateAdded: "2020-07-08 01:26:02",
+    isMain: false
+  },
+  {
+    id: "2",
+    title: "Best SNES RPGs You’ve Already Seen on Hundreds of Lists Already",
+    thumbnail: "https://gamefaqs1.cbsistatic.com/box/2/2/1/24221_front.jpg",
+    games: "14",
+    comments: "1",
+    dateAdded: "2020-04-23 18:33:18",
+    isMain: false
+  },
+  {
+    id: "3",
+    title: "Best Shmups for REAL MANLY MEN!!",
+    thumbnail: "https://gamefaqs1.cbsistatic.com/box/2/0/2/49202_front.jpg",
+    games: "27",
+    comments: "4",
+    dateAdded: "2020-05-29 18:39:50",
+    isMain: false
+  },
+  {
+    id: "4",
+    title: "Legendary Soundtracks",
+    thumbnail: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2cm4.jpg",
+    games: "39",
+    comments: "1",
+    dateAdded: "2020-04-25 04:39:28",
+    isMain: false
+  },
+  {
+    id: "5",
+    title: "Legendary Soundtracks 2",
+    thumbnail: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2cm4.jpg",
+    games: "39",
+    comments: "1",
+    dateAdded: "2020-04-25 04:39:28",
+    isMain: false
+  },
+  {
+    id: "6",
+    title: "Legendary Soundtracks 3",
+    thumbnail: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2cm4.jpg",
+    games: "39",
+    comments: "1",
+    dateAdded: "2020-04-25 04:39:28",
+    isMain: false
+  },
+  {
+    id: "7",
+    title: "Legendary Soundtracks 4",
+    thumbnail: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2cm4.jpg",
+    games: "39",
+    comments: "1",
+    dateAdded: "2020-04-25 04:39:28",
+    isMain: false
+  },
+  {
+    id: "8",
+    title: "Legendary Soundtracks 5",
+    thumbnail: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2cm4.jpg",
+    games: "39",
+    comments: "1",
+    dateAdded: "2020-04-25 04:39:28",
+    isMain: false
+  },
+  {
+    id: "9",
+    title: "Legendary Soundtracks 6",
+    thumbnail: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2cm4.jpg",
+    games: "39",
+    comments: "1",
+    dateAdded: "2020-04-25 04:39:28",
+    isMain: false
+  },
+  {
+    id: "10",
+    title: "Legendary Soundtracks 7",
+    thumbnail: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2cm4.jpg",
+    games: "39",
+    comments: "1",
+    dateAdded: "2020-04-25 04:39:28",
+    isMain: false
+  },
+];
+
+export default collectionListData;
